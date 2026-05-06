81822e76304d0a9373a9dcee7afa3147e950bdc8
nwaf67660-cyber <nwaf67660@gmail.com>
2026-05-06 20:06:20 +0300

    Update GoogleDriveEpisodes.kt

--- lib/googledrive-episodes/src/main/java/eu/kanade/tachiyomi/lib/googledriveepisodes/GoogleDriveEpisodes.kt
+++ lib/googledrive-episodes/src/main/java/eu/kanade/tachiyomi/lib/googledriveepisodes/GoogleDriveEpisodes.kt
@@ -14,7 +14,7 @@ import okhttp3.RequestBody.Companion.toRequestBody
 import java.security.MessageDigest
 
 class GoogleDriveEpisodes(private val client: OkHttpClient, private val headers: Headers) {
-    // Lots of code borrowed from https://github.com/yt-dlp/yt-dlp/blob/master/yt_dlp/extractor/googledrive.py under the `GoogleDriveFolderIE` class
+
     fun getEpisodesFromFolder(folderId: String, path: String, maxRecDepth: Int, trimNames: Boolean): List<SEpisode> {
         val episodeList = mutableListOf<SEpisode>()
 
@@ -33,15 +33,17 @@ class GoogleDriveEpisodes(private val client: OkHttpClient, private val headers:
             ).execute().asJsoup()
             if (driveDocument.selectFirst("title:contains(Error 404 \\(Not found\\))") != null) return
 
-            val keyScript = drivescript").first { script ->
+            // FIX: كان "drivescript").first" والصح "driveDocument.select("script").first"
+            val keyScript = driveDocument.select("script").first { script ->
                 KEY_REGEX.find(script.data()) != null
             }.data()
             val key = KEY_REGEX.find(keyScript)?.groupValues?.get(1) ?: ""
 
             val versionScript = driveDocument.select("script").first { script ->
-                KEY_REGEX.find(script.data()) != null
+                VERSION_REGEX.find(script.data()) != null
             }.data()
             val driveVersion = VERSION_REGEX.find(versionScript)?.groupValues?.get(1) ?: ""
+
             val sapisid = client.cookieJar.loadForRequest("https://drive.google.com".toHttpUrl()).firstOrNull {
                 it.name == "SAPISID" || it.name == "__Secure-3PAPISID"
             }?.value ?: ""
@@ -49,6 +51,7 @@ class GoogleDriveEpisodes(private val client: OkHttpClient, private val headers:
             var pageToken: String? = ""
             while (pageToken != null) {
                 val requestUrl = "/drive/v2internal/files?openDrive=false&reason=102&syncType=0&errorRecovery=false&q=trashed%20%3D%20false%20and%20'$folderId'%20in%20parents&fields=kind%2CnextPageToken%2Citems(kind%2CmodifiedDate%2ChasVisitorPermissions%2CcontainsUnsubscribedChildren%2CmodifiedByMeDate%2ClastViewedByMeDate%2CalternateLink%2CfileSize%2Cowners(kind%2CpermissionId%2CemailAddressFromAccount%2Cdomain%2Cid)%2ClastModifyingUser(kind%2CpermissionId%2CemailAddressFromAccount%2Cid)%2CcustomerId%2CancestorHasAugmentedPermissions%2ChasThumbnail%2CthumbnailVersion%2Ctitle%2Cid%2CresourceKey%2CabuseIsAppealable%2CabuseNoticeReason%2Cshared%2CaccessRequestsCount%2CsharedWithMeDate%2CuserPermission(role)%2CexplicitlyTrashed%2CmimeType%2CquotaBytesUsed%2Ccopyable%2Csubscribed%2CfolderColor%2ChasChildFolders%2CfileExtension%2CprimarySyncParentId%2CsharingUser(kind%2CpermissionId%2CemailAddressFromAccount%2Cid)%2CflaggedForAbuse%2CfolderFeatures%2Cspaces%2CsourceAppId%2Crecency%2CrecencyReason%2Cversion%2CactionItems%2CteamDriveId%2ChasAugmentedPermissions%2CcreatedDate%2CprimaryDomainName%2CorganizationDisplayName%2CpassivelySubscribed%2CtrashingUser(kind%2CpermissionId%2CemailAddressFromAccount%2Cid)%2CtrashedDate%2Cparents(id)%2Ccapabilities(canMoveItemIntoTeamDrive%2CcanUntrash%2CcanMoveItemWithinTeamDrive%2CcanMoveItemOutOfTeamDrive%2CcanDeleteChildren%2CcanTrashChildren%2CcanRequestApproval%2CcanReadCategoryMetadata%2CcanEditCategoryMetadata%2CcanAddMyDriveParent%2CcanRemoveMyDriveParent%2CcanShareChildFiles%2CcanShareChildFolders%2CcanRead%2CcanMoveItemWithinDrive%2CcanMoveChildrenWithinDrive%2CcanAddFolderFromAnotherDrive%2CcanChangeSecurityUpdateEnabled%2CcanBlockOwner%2CcanReportSpamOrAbuse%2CcanCopy%2CcanDownload%2CcanEdit%2CcanAddChildren%2CcanDelete%2CcanRemoveChildren%2CcanShare%2CcanTrash%2CcanRename%2CcanReadTeamDrive%2CcanMoveTeamDriveItem)%2CcontentRestrictions(readOnly)%2CapprovalMetadata(approvalVersion%2CapprovalSummaries%2ChasIncomingApproval)%2CshortcutDetails(targetId%2CtargetMimeType%2CtargetLookupStatus%2CtargetFile%2CcanRequestAccessToTarget)%2CspamMetadata(markedAsSpamDate%2CinSpamView)%2Clabels(starred%2Ctrashed%2Crestricted%2Cviewed))%2CincompleteSearch&appDataFilter=NO_APP_DATA&spaces=drive&pageToken=$pageToken&maxResults=100&supportsTeamDrives=true&includeItemsFromAllDrives=true&corpora=default&orderBy=folder%2Ctitle_natural%20asc&retryCount=0&key=$key HTTP/1.1"
+
                 val body = """--$BOUNDARY
                     |content-type: application/http
                     |content-transfer-encoding: binary
@@ -81,23 +84,23 @@ class GoogleDriveEpisodes(private val client: OkHttpClient, private val headers:
                 }
 
                 if (parsed.items == null) throw Exception("Failed to load items, please log in to google drive through webview")
-                parsed.items.forEachIndexed { index, it ->
-                    if (it.mimeType.startsWith("video")) {
-                        val size = it.fileSize?.toLongOrNull()?.let { formatBytes(it) }
-                        val pathName = path.trimInfo()
 
+                parsed.items.forEachIndexed { index, item ->
+                    if (item.mimeType.startsWith("video")) {
+                        val size = item.fileSize?.toLongOrNull()?.let { formatBytes(it) }
+                        val pathName = path.trimInfo()
                         episodeList.add(
                             SEpisode.create().apply {
-                                name = if (trimNames) it.title.trimInfo() else it.title
-                                this.url = "https://drive.google.com/uc?id=${it.id}"
-                                episode_number = ITEM_NUMBER_REGEX.find(it.title.trimInfo())?.groupValues?.get(1)?.toFloatOrNull() ?: index.toFloat()
+                                name = if (trimNames) item.title.trimInfo() else item.title
+                                url = "https://drive.google.com/uc?id=${item.id}"
+                                episode_number = ITEM_NUMBER_REGEX.find(item.title.trimInfo())?.groupValues?.get(1)?.toFloatOrNull() ?: index.toFloat()
                                 date_upload = -1L
                                 scanlator = "$size • /$pathName"
                             },
                         )
                     }
-                    if (it.mimeType.endsWith(".folder")) {
-                        traverseFolder(it.id, "$path/${it.title}", recursionDepth + 1)
+                    if (item.mimeType.endsWith(".folder")) {
+                        traverseFolder(item.id, "$path/${item.title}", recursionDepth + 1)
                     }
                 }
 
@@ -106,14 +109,11 @@ class GoogleDriveEpisodes(private val client: OkHttpClient, private val headers:
         }
 
         traverseFolder(folderId, path)
-
         return episodeList
     }
 
-    // https://github.com/yt-dlp/yt-dlp/blob/8f0be90ecb3b8d862397177bb226f17b245ef933/yt_dlp/extractor/youtube.py#L573
     private fun generateSapisidhashHeader(SAPISID: String, origin: String = "https://drive.google.com"): String {
         val timeNow = System.currentTimeMillis() / 1000
-        // SAPISIDHASH algorithm from https://stackoverflow.com/a/32065323
         val sapisidhash = MessageDigest
             .getInstance("SHA-1")
             .digest("$timeNow $SAPISID $origin".toByteArray())
@@ -138,13 +138,11 @@ class GoogleDriveEpisodes(private val client: OkHttpClient, private val headers:
     private fun String.trimInfo(): String {
         var newString = this.replaceFirst("""^\[\w+\] ?""".toRegex(), "")
         val regex = """( ?\[[\s\w-]+\]| ?\([\s\w-]+\))(\.mkv|\.mp4|\.avi)?${'$'}""".toRegex()
-
         while (regex.containsMatchIn(newString)) {
             newString = regex.replace(newString) { matchResult ->
                 matchResult.groups[2]?.value ?: ""
             }
         }
-
         return newString.trim()
     }
 
